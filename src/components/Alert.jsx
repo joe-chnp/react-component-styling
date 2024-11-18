77f02d3990b93e4c737d68fedf6fa94c36e02c34
Joe <chanaphat.sip@gmail.com>
2024-11-19 06:45:15 +0700

    fe-3-joe

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,46 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 
-function Alert() {}
+
+function Alert(props) {
+  let bg;
+  let img;
+  if (props.type === 'error') {
+    bg = '#F9C8C8';
+    img = '../../public/errorAlert.svg';
+  } else if(props.type === 'warning') {
+    bg = '#F9D9C8';
+    img = '../../public/warningAlert.svg';
+  } else if(props.type === 'info') {
+    bg = '#F9EBC8';
+    img = '../../public/infoAlert.svg';
+  } else if(props.type === 'success') {
+    bg = '#CEF7CD';
+    img = '../../public/successAlert.svg';
+  }; 
+  
+  return (
+    <div 
+      css={css`
+        background-color: ${bg};
+        width: 650px;
+        height: 76px;
+        border-radius: 10px;
+        font-size: 20px;
+        color: #444444;
+        display: flex;
+        align-items: center;
+        gap: 15px;
+        padding-left: 20px;
+        box-sizing: border-box;
+        font-family: "Kanit";
+        font-weight: 700;
+      `}
+    >
+      <img src={img}/>
+      <span>This is {props.type} alert box</span>
+    </div>
+  )
+}
+
+export default Alert
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,30 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 
-function Button() {}
+function Button(props) {
+  let bg;
+  if (props.type === 'primary') {
+    bg = '#074EE8';
+  } else if ( props.type === 'secondary') {
+    bg = '#07A4E8';
+  }
+  return (
+    <button
+      css={css`
+        background-color: ${bg};
+        width: 171.19px;
+        height: 50px;
+        border-radius: 4px;
+        padding: 16px 0;
+        color: #FFFFFF;
+        font-family: "Sarabun";
+        font-weight: 400;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      `}
+     >Button</button>
+  )
+}
+
+export default Button
